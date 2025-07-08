911a3351c0ea0c140cc29aab5e4218ae9d6e3010
Aniket Bhattacharya <aniket.bhattacharya1729@gmail.com>
2025-07-08 20:28:45 +0530

    minor fixes

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 
 const allowedOrigins = [
   'https://crash-canvas.vercel.app',
-  'https://crash-canvas-rhlx5n5ld-aniket-bhattacharyas-projects.vercel.app'
+  'https://crash-canvas-mh15vmj85-aniket-bhattacharyas-projects.vercel.app'
 ];
 
 app.use(cors({
